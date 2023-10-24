2aea873b6244175be9fab229c852c26a98148b10
JH <dafr47@naver.com>
2023-10-24 11:12:20 +0900

    react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { Fragment } from "react";
 
 function App() {
+  const member = {
+    name: "james",
+    age: 22,
+    addr: "Newyork"
+  };
+  const style = {
+    backgroundColor: "#222",
+    color: "royalblue",
+    fontSize: "2em"
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 style={{color:"red"}}> hello my name is {member.name}, age is {member.age}, address is {member.addr}</h1>
+      {/* 조건부 랜더링 : 3항 연산자 사용하는 방법 */}
+      {member.name === "리액트" ? <h1>this is react</h1> : <h1>no react</h1>}
+      {/* 조건부 랜더링 : AND 연산자 사용 */}
+      {member && <h1 style={style}>{member.name} has been logged in</h1>}
+      <div className="App">
+        <p className="title-name"></p>
+        <p className="title-name"></p>
+        <p className="title-name"></p>
+        <p className="title-name"></p>
+      </div>
+      <br/>
+    </>
   );
 }
 
